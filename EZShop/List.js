a4803ac7155f8bbca84d9863b2f779b8698d1bc7
Stormwindsky <83926426+Stormwindsky@users.noreply.github.com>
2025-04-08 08:19:22 -0400

    Update List.js

--- EZShop/List.js
+++ EZShop/List.js
@@ -3,7 +3,7 @@ window.gameList = [
     {
         title: "FNAF 1 Custom Map for Among Us",
         developer: "By Stormwindsky",
-        thumbnail: "https://web.archive.org/web/20250404110031if_/https://m.gjcdn.net/game-thumbnail/1000/964789-tavicsmx-v4.webp",
+        thumbnail: "https://m.gjcdn.net/game-thumbnail/1000/964789-tavicsmx-v4.webp",
         downloadLink: "https://gamejolt.com/games/FNAF1CustomMapForAmongUs/964789",
         formats: [".lim2"],
         downloadText: "Download on GameJolt"
@@ -11,7 +11,7 @@ window.gameList = [
     {
         title: "This Japanese company can't ban this “Fan Game”",
         developer: "By Stormwindsky",
-        thumbnail: "https://web.archive.org/web/20250404111034if_/https://m.gjcdn.net/game-thumbnail/900/962820-crop0_179_960_719-nj6jtrfw-v4.webp",
+        thumbnail: "https://m.gjcdn.net/game-thumbnail/900/962820-crop0_179_960_719-nj6jtrfw-v4.webp",
         downloadLink: "https://gamejolt.com/games/TJccbtFG/962820",
         formats: [".exe", ".deb", ".sb3"],
         downloadText: "Download on GameJolt"
@@ -19,7 +19,7 @@ window.gameList = [
     {
         title: "Among Mods",
         developer: "By Stormwindsky",
-        thumbnail: "https://web.archive.org/web/20250404115637if_/https://m.gjcdn.net/game-screenshot/300/39067255-csce4rd7-v4.webp",
+        thumbnail: "https://m.gjcdn.net/game-screenshot/300/39067255-csce4rd7-v4.webp",
         downloadLink: "https://github.com/Stormwindsky/Among-Mods",
         formats: [".py"],
         downloadText: "Download on Github"
